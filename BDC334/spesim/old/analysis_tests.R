911b710a75d174d4c89aea9e639a556e53099857
AJ Smit <ajsmit@uwc.ac.za>
2025-08-18 07:32:34 +0200

    Rebuild.

--- BDC334/spesim/old/analysis_tests.R
+++
@@ -1,9 +0,0 @@
-library(tidyverse)
-
-# Load the dataset
-demo_spp <- read.csv("BDC334/spesim/demo_output_20250806_212011_abundances.csv") |>
-  select(-site)
-
-# Calculate a species association matrix
-demo_spp |>
-  cor()
